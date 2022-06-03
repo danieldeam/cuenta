766a6eb469769bc132934319b2d68800ac9da638
danieldeam <93274949+danieldeam@users.noreply.github.com>
2022-06-03 11:06:33 -0500

    Se agrega el proyecto

---
+++ app/src/main/java/com/tiendasena/cuenta/notificaciones.java
@@ -0,0 +1,14 @@
+package com.tiendasena.cuenta;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class notificaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notificaciones);
+    }
+}
---
+++ app/src/main/java/com/tiendasena/cuenta/producto.java
@@ -0,0 +1,39 @@
+package com.tiendasena.cuenta;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.tiendasena.cuenta.databinding.ActivityProductoBinding;
+
+public class producto extends AppCompatActivity {
+
+    private ActivityProductoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityProductoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
